6e13296a151d7793ce975be1eabf6f0b8f28c0f2
rchan321 <97616168+rchan321@users.noreply.github.com>
2022-07-12 15:05:36 -0400

    deleted develop folder

--- models/index.js
+++ models/index.js
@@ -22,7 +22,7 @@ Product.belongsToMany(Tag, {
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'product_tag',
+  as: '',
   foreignKey: 'tag_id',
 });
 
